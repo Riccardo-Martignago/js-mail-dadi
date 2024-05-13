9119a71cd6d6886eb100b8e4e0984eeadc497f03
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-13 21:08:10 +0200

    Completato esercizio bonus dove inserisco 6 numeri e se paru vengono stampati nell'array

--- Saltare a pié dispari [Bonus]/JS/script.js
+++ Saltare a pié dispari [Bonus]/JS/script.js
@@ -1,1 +1,11 @@
-console.log('Hello!')
+const array = []
+for(let i = 0; i < 6; i++){
+    let myNumber = Number.parseInt (prompt ('Digita un numero'))
+    if(myNumber % 2 === 0){
+        array.push(myNumber)
+    }
+    else{
+        console.log('Questo numero é dispari', myNumber)
+    }
+}
+console.log(array)
