2b90eee466ed4e525e6eab64362a60310a78ec04
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-13 19:45:22 +0200

    Esercizio del gioco dei dadi eseguito con stampa dei risultati in console

--- Gioco dei dadi/JS/script.js
+++ Gioco dei dadi/JS/script.js
@@ -1,1 +1,14 @@
-console.log('Hello!')
+const myNumber = Number.parseInt (Math.random() * 6 + 1)
+const pcNumber = Number.parseInt (Math.random() * 6 + 1)
+if(myNumber === pcNumber){
+    console.log("Un pareggi non é contemplato, ricarica la pagina!")
+    console.log(myNumber,  pcNumber)
+}
+else if (myNumber > pcNumber){
+    console.log("L'uomo ha vinto sulla macchina!")
+    console.log(myNumber,  pcNumber)
+}
+else{
+    console.log("Le macchine sono piu forti!")
+    console.log(myNumber,  pcNumber)
+}
