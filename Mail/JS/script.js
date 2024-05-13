b31501b3cd723f317b882c72a89f8a16e1491a0e
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-13 20:04:17 +0200

    Esercizio Mail completato con stampa nella console se l'accesso é consentito oppure no

--- Mail/JS/script.js
+++ Mail/JS/script.js
@@ -1,1 +1,8 @@
-console.log('Hello!')
+const email = prompt('Inserisci la tua email')
+const listaEmail = ['cribbio@speedy.com', 'capperosottoaceto80@frittella.nt', 'calamarofritto43@griglia.up']
+if (listaEmail.includes(email)){
+    console.log('Puo accedere alla pagina!')
+}
+else{
+    console.log("Non puo accedere alla pagina, l'email non corrisponde a un email valida!")
+}
